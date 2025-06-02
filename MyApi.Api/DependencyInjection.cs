67f6bec058496acf173a07c4d8c2ddc6d1bd815b
Lê Yên <140315513+leyen1403@users.noreply.github.com>
2025-06-02 15:18:54 +0700

    feat(api): tạo project ASP.NET Core Clean Architecture và thêm DI cho các module

---
+++ MyApi.Api/DependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿using MyApi.Application;
+using MyApi.Infranstructure;
+
+namespace MyApi.Api
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddAppDI(this IServiceCollection services)
+        {
+            services.AddApplicationDI()
+                .AddInfranstructureDI();
+            return services;
+        }
+    }
+}
---
+++ MyApi.Infranstructure/DependencyInfection.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyApi.Infranstructure
+{
+    public static class DependencyInfection
+    {
+        public static IServiceCollection AddInfranstructureDI(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
